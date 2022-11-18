f2db1d2bfb6c955e732400cac75e0f9b6c6c3a65
Miri <mirifurman@gmail.com>
2022-11-18 19:53:49 +0200

    18.11.22

--- src/pages/ShoePage.js
+++ src/pages/ShoePage.js
@@ -1,4 +1,4 @@
-import { useParams ,Link} from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -7,8 +7,12 @@ export default function ShoePage(props) {
     const [specificShoeData, setSpecificShoeData] = useState([])
     const [errorMessage, setErrorMessage] = useState("")
     const [isLoading, setIsLoading] = useState(false)
-    const [isDeleted,setIsDeleted]=useState(false)
-
+    const [isDeleted, setIsDeleted] = useState(false)
+    const [isEditing, setIsEditing] = useState(false)
+    const [brand, setBrand] = useState("")
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
 
 
 
@@ -23,6 +27,7 @@ export default function ShoePage(props) {
                 setSpecificShoeData(data);
                 setIsLoading(false);
 
+
             } catch (e) {
                 setErrorMessage(e.message);
                 setTimeout(() => {
@@ -33,7 +38,7 @@ export default function ShoePage(props) {
         fetchData();
     }, [shoeid.shoeid]);
 
-//*delete*//
+    //*delete*//
     const handleDelete = async (shoeid) => {
         try {
             const { data } = await axios.delete(
@@ -46,30 +51,83 @@ export default function ShoePage(props) {
                 setErrorMessage(null);
             }, 30000);
         }
-setIsDeleted(true)
+        setIsDeleted(true)
     };
 
 
+    //* edit*//
+    function handleEditClick(shoeid) {
+        setIsEditing(true)
+        console.log(shoeid.shoeid);
+        setBrand(specificShoeData.brand)
+        setName(specificShoeData.name)
+        setPrice(specificShoeData.price)
+        setImage(specificShoeData.image)
+    }
+
+
+    // function handleEditSubmit(e) {
+    //     e.preventDefault();
+    //     console.log(shoeid.shoeid);
+    //     handleUpdate(shoeid)
+
+    // }
+
+    // async function handleUpdate() {
+    //     try {
+    //         console.log(shoeid);
+    //         const { data } = await axios.put(`https://637389a9348e9472990f6169.mockapi.io/shoes/${shoeid.shoeid}`, {
+    //             brand: brand,
+    //             name: name,
+    //             price: price,
+    //             image: image
+    //         });
+    //         setIsEditing(false)
+    //     } catch (e) {
+    //         setErrorMessage(e.message);
+    //         setTimeout(() => {
+    //             setErrorMessage(null);
+    //         }, 1500);
+    //     }
+
+    // }
+
+
+
+
     return (<div className="shoePage">
+        {isEditing ? <form >
+            <label htmlFor="brand"> Brand</label>
+            <input type="text" value={brand} onChange={({ target: { value } }) => setBrand(value)}></input>
+            <label htmlFor="name"> Name</label>
+            <input type="text" value={name} onChange={({ target: { value } }) => setName(value)}></input>
+            <label htmlFor="price"> Price</label>
+            <input type="number" name="price" value={price} onChange={({ target: { value } }) => setPrice(value)}></input>
+            <label htmlFor="image"> Image</label>
+            <input type="text" name="image" value={image} onChange={({ target: { value } }) => setImage(value)}></input>
+            <button type="submit" onSubmit={(e)=>{e.preventDefault()
+                console.log(e)}}>Save</button>
+            <button>cancel</button>
+        </form> : ""}
         {isLoading && <h1 className='spinner'>loading</h1>}
         {errorMessage ? <h4>{errorMessage}</h4> : ""}
-        {specificShoeData.length !== 0 ?
+        {specificShoeData.length !== 0 && !isEditing ?
             <div className="shoePageDiv">
-                <h4>{specificShoeData.length}</h4>
                 <h3>{specificShoeData.brand}</h3>
                 <h3>{specificShoeData.name}</h3>
                 <h3>price:{specificShoeData.price}$</h3>
                 <img src={specificShoeData.image} alt="" width="350px" ></img>
-                <button>edit</button>
+                <button onClick={() => { handleEditClick(shoeid) }}>edit</button>
                 <button onClick={() => {
                     handleDelete(shoeid);
                 }}>delete</button>
             </div> : ""
         }
-        {isDeleted?<div> <h4>Item deleted successfully</h4>
-        <Link to='/shoespage/'>Return to All shoes</Link>
-         </div>
-       :""}
+        {isDeleted ? <div> <h4>Item deleted successfully</h4>
+            <Link to='/shoespage/'>Return to All shoes</Link>
+        </div>
+            : ""}
+
 
 
     </div>)
